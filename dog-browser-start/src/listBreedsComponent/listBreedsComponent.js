e23b15ed80ff90099d031426ff580bfdb531f944
Emnon <csampaie.5@gmail.com>
2021-06-08 12:19:32 +0200

    2_1

--- dog-browser-start/src/listBreedsComponent/listBreedsComponent.js
+++ dog-browser-start/src/listBreedsComponent/listBreedsComponent.js
@@ -1,21 +1,55 @@
 import '../css/listBreedsComponent.css';
 import ContentComponent from '../contentComponent/contentComponent';
 
+
+
 class ListBreeds extends ContentComponent {
   constructor() {
     super();
     this.render();
   }
-  
+
+
+
   async getFullList() {
-    const response = await fetch('https://dog.ceo/api/breeds/list/all');
-    if(response.status === 404) {
-      this.displayError('Page not found!');
-      return;
+    if (localStorage.length === 0) {
+      const response = await fetch('https://dog.ceo/api/breeds/list/all');
+      if (response.status === 404) {
+        this.displayError('Page not found!');
+        return;
+      }
+      const data = await response.json();
+      console.log(data);
+
+      let key;
+      let value;
+      for (const breed in data.message) {
+
+        // ha a value (ami egy tömb) hossza nem nulla
+        if (data.message[breed].length !== 0) {
+          // akkor végmegyünk a tömbön, és kiírjuk a fajtákat, alfajjal együtt,
+          for (const subBreed of data.message[breed]) {
+            // minden alfaj mögé odaírjuk a főfaj nevét... pl: 
+            // boston bulldog, french bulldog, stb...
+            key = subBreed;
+            value = breed;
+            localStorage.setItem(key, value);
+          }
+        } else {
+          // ha nincs alfaj (a tömb hossza nulla)
+          // akkor csak a főfajt jelenítjük meg
+          key = breed;
+          value = "";
+          localStorage.setItem(key, value);
+        }
+      }
+
+
+
+      return data;
     }
-    const data = await response.json();
-    return data;
   }
+
 
   createListItem(title) {
     const item = document.createElement('div');
@@ -28,12 +62,12 @@ class ListBreeds extends ContentComponent {
     // a result.message egy object, amin végig megyünk key:value páronként..
     for (const breed in results.message) {
       // ha a value (ami egy tömb) hossza nem nulla
-      if(results.message[breed].length !== 0) {
+      if (results.message[breed].length !== 0) {
         // akkor végmegyünk a tömbön, és kiírjuk a fajtákat, alfajjal együtt,
-        for(const subBreed of results.message[breed]) {
+        for (const subBreed of results.message[breed]) {
           // minden alfaj mögé odaírjuk a főfaj nevét... pl: 
           // boston bulldog, french bulldog, stb...
-          this.createListItem(subBreed+' '+breed);
+          this.createListItem(subBreed + ' ' + breed);
         }
       } else {
         // ha nincs alfaj (a tömb hossza nulla)
@@ -43,15 +77,28 @@ class ListBreeds extends ContentComponent {
     }
   }
 
+  displayListFromLocalStorage() {
+    for (let i = 0; i < localStorage.length; i++) {
+      let key = localStorage.key(i);
+      let value = localStorage.getItem(key);
+      this.createListItem(key + ' ' + value);
+    }
+  }
+
   render() {
     const button = document.createElement('button');
     button.classList.add('list-button');
     button.innerHTML = 'List Breeds';
+
     // button html elemenek van onclick attribútuma...
     button.onclick = () => {
       this.clearContent();
-      //                                           👇🏻short circuit evaluation
-      this.getFullList().then( results => { results && this.displayList(results); });
+      if (localStorage.length !== 0) {
+        this.displayListFromLocalStorage();
+      } else {
+        //                                           👇🏻short circuit evaluation
+        this.getFullList().then(results => { results && this.displayList(results); });
+      }
     };
     document.querySelector('#header').appendChild(button);
   }
